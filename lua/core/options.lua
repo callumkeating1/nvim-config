a880b6cfb20a67f4d75a69311222a984ed9b037a
callumkeating1 <CallumKeating124@proton.me>
2025-07-06 18:34:04 +1000

    changed indent side to 4

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,1 +1,5 @@
 vim.o.shell = "C:\\Program Files\\Git\\bin\\bash.exe"
+vim.opt.tabstop = 4       -- number of visual spaces per TAB
+vim.opt.shiftwidth = 4    -- number of spaces to use for each step of (auto)indent
+vim.opt.expandtab = true  -- convert tabs to spaces
+vim.expandtab = "4"
